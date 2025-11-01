8021f011075a1736b8659a36c3d74b70709a2e99
UmerMughal <umermughal4554@gmail.com>
2025-11-01 09:32:09 +0500

    dfd

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Experience from "./pages/Experience";
 import Contact from "./pages/Contant";
 import CustomCursor from "./components/CustomCursor";
 import ScrollToTop from "./components/ScrollToTop";
@@ -27,7 +26,6 @@ export default function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
-              <Route path="/experience" element={<Experience />} />
               <Route path="/contact" element={<Contact />} />
             </Routes>
           </main>
